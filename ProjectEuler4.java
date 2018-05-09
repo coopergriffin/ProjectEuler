c694ee8e42823f2c69e0cfedd7e9d783e9101f4b
Cooper Griffin <coopergriffin@hotmail.com>
2018-05-09 17:54:49 -0400

    Updated info a top of files

--- ProjectEuler4.java
+++ ProjectEuler4.java
@@ -1,3 +1,11 @@
+//Cooper Griffin
+//April 15th 2018
+//Project Euler 2 Largest Palindrome Product 
+/*A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+Find the largest palindrome made from the product of two 3-digit numbers.*/
+
+//Answer: 906609
+
 public class ProjectEuler4{
 	public static void main(String[] args){
 		int[] x = productTwoThreeDigits();
--- ProjectEuler5.java
+++ ProjectEuler5.java
@@ -1,3 +1,10 @@
+//Cooper Griffin
+//April 15th 2018
+//Project Euler 5 Smallest multiple
+/*2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?*/
+
+
 public class ProjectEuler5{
 	public static void main(String[] args){
 
